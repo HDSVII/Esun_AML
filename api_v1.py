cd7133302c2b467d39e92ca1f3210741e34abc5b
kairx772 <laoweitse@elolab-nthu.org>
2020-07-21 23:12:21 +0800

    Chnage port to 8081.

--- api_v1.py
+++ api_v1.py
@@ -115,4 +115,4 @@ def inference():
     return jsonify(return_data)
 
 if __name__ == "__main__":    
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=True)
